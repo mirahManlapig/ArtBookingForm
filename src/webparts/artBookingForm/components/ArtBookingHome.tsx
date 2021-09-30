d78bdc26d03d2c1ae5a87fa4165c82299c739e48
mirahManlapig <mirah.manlapig@gmail.com>
2021-09-30 19:33:41 +0800

    initial commit

--- src/webparts/artBookingForm/components/ArtBookingHome.tsx
+++ src/webparts/artBookingForm/components/ArtBookingHome.tsx
@@ -7,10 +7,13 @@ import { sp } from "@pnp/sp";
 export const HomePage = (props) => {
   const [bookings, setBookings] = useState([]);
   const [status, setStatus] = useState("ready");
+  const [isDisabledNewBookBtn, setIsDisabledNewBookBtn] = useState(false);
+  const [activeBookingDate, setActiveBookingDate] = useState(null);
   const fetchData = async () => {
     try {
       setStatus("loading");
       await App.getBookings(props.listName).then((bookingsList) => {
+        console.log(bookingsList);
         setBookings(bookingsList);
       });
       setStatus("ready");
@@ -34,6 +37,7 @@ export const HomePage = (props) => {
         listName={props.listName}
         bookings={bookings}
         status={status}
+        isFormAvailable={!isDisabledNewBookBtn}
       />
     </div>
   );
